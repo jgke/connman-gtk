e48d23cefbee14f80abab4ee49d5ce8f6e258c41
Jaakko Hannikainen <jaakko.hannikainen@inte.com>
2015-09-17 09:41:55 +0300

    status: Add basic menu to the icon

--- src/status.c
+++ src/status.c
@@ -19,13 +19,38 @@
 */
 
 #include <gtk/gtk.h>
+#include <glib/gi18n.h>
 
+#include "main.h"
 #include "status.h"
 
 #ifdef USE_STATUS_ICON
-static void status_activate(GtkStatusIcon *icon, gpointer user_data)
+static void status_activate(gpointer *ignored, gpointer user_data)
 {
 	g_signal_emit_by_name(user_data, "activate");
+}
+
+static void status_exit(gpointer *ignored, gpointer user_data)
+{
+	g_signal_emit_by_name(main_window, "destroy");
+}
+
+static void status_menu(GtkStatusIcon *icon, guint button, guint activate_time,
+                        gpointer user_data)
+{
+	GtkMenu *menu;
+	GtkWidget *open, *exit;
+	menu = GTK_MENU(gtk_menu_new());
+
+	open = gtk_menu_item_new_with_label(_("Open app"));
+	exit = gtk_menu_item_new_with_label(_("Exit"));
+	g_signal_connect(open, "activate", G_CALLBACK(status_activate),
+			 user_data);
+	g_signal_connect(exit, "activate", G_CALLBACK(status_exit), user_data);
+	gtk_container_add(GTK_CONTAINER(menu), open);
+	gtk_container_add(GTK_CONTAINER(menu), exit);
+	gtk_widget_show_all(GTK_WIDGET(menu));
+	gtk_menu_popup(menu, NULL, NULL, NULL, NULL, button, activate_time);
 }
 
 void status_init(GtkApplication *app)
@@ -34,6 +59,7 @@ void status_init(GtkApplication *app)
 
 	icon = gtk_status_icon_new_from_icon_name("preferences-system-network");
 	g_signal_connect(icon, "activate", G_CALLBACK(status_activate), app);
+	g_signal_connect(icon, "popup-menu", G_CALLBACK(status_menu), app);
 }
 #else
 void status_init(GtkApplication *app)
